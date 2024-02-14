2fa0901cea489e7cbc1e15dc687bb2598e945c68
Chhavi Gupta <chhavigupta1705@gmail.com>
2024-02-14 11:55:42 +0530

    learning rust

---
+++ var_pract/target/debug/deps/var_pract.d
@@ -0,0 +1,5 @@
+C:\Rust Development\var_pract\target\debug\deps\var_pract.exe: src\main.rs
+
+C:\Rust Development\var_pract\target\debug\deps\var_pract.d: src\main.rs
+
+src\main.rs:
---
+++ var_pract/target/debug/var_pract.d
@@ -0,0 +1,1 @@
+C:\Rust\ Development\var_pract\target\debug\var_pract.exe: C:\Rust\ Development\var_pract\src\main.rs
